81825b7e847e24838c4c2a1e562a9a81716652b3
AlexLenitschek <alexander.lenitschek@gmx.de>
2025-02-23 10:32:26 +0100

    Adding Chapter09. File Systems and Paths

---
+++ Program.Helpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+partial class Program
+    {
+        private static void SectionTitle(string title)
+        {
+            WriteLine();
+            ConsoleColor previousColor = ForegroundColor; // safe the default color
+            ForegroundColor = ConsoleColor.DarkRed;
+            WriteLine($"*** {title} ***");
+            ForegroundColor = previousColor; // restore the default color
+        }
+    }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,171 @@
-﻿
+﻿using Spectre.Console;
+using System.Reflection.PortableExecutable;
+
+// Note: Personal Special Folder at: C:\Users\Alex\OneDrive\Dokumente
+
+#region Handling Cross-platform environments and filesystems
+
+SectionTitle("Handling cross-platform environments and filesystems");
+
+// Create a Spectre Console Table.
+Table table = new();
+
+// Add two columns with markup for colors.
+table.AddColumn("[blue]MEMBER[/]");
+table.AddColumn("[blue]VALUE[/]");
+
+// Add rows.
+table.AddRow("Path.PathSeparator", PathSeparator.ToString());
+table.AddRow("Path.DirectorySeparatorChar", DirectorySeparatorChar.ToString());
+table.AddRow("Directory.GetCurrentDirectory()", GetCurrentDirectory());
+table.AddRow("Environment.CurrentDirectory", CurrentDirectory);
+table.AddRow("Environment.SystemDirectory", SystemDirectory);
+table.AddRow("Path.GetTempPath()", GetTempPath());
+table.AddRow("");
+table.AddRow("GetFolderPath(SpecialFolder", "");
+table.AddRow("  .System)", GetFolderPath(SpecialFolder.System));
+table.AddRow("  .ApplicationData)",
+  GetFolderPath(SpecialFolder.ApplicationData));
+table.AddRow("  .MyDocuments)",
+  GetFolderPath(SpecialFolder.MyDocuments));
+table.AddRow("  .Personal)",
+  GetFolderPath(SpecialFolder.Personal));
+
+
+AnsiConsole.Write(table);
+WriteLine();
+#endregion
+
+
+#region Managing drives
+
+SectionTitle("Managing drives");
+
+Table drives = new();
+
+drives.AddColumn("[blue]NAME[/]");
+drives.AddColumn("[blue]TYPE[/]");
+drives.AddColumn(new TableColumn("[blue]FORMAT[/]"));
+drives.AddColumn(new TableColumn(
+  "[blue]SIZE (BYTES)[/]").RightAligned());
+drives.AddColumn(new TableColumn(
+  "[blue]FREE SPACE[/]").RightAligned());
+
+foreach (DriveInfo drive in DriveInfo.GetDrives())
+{
+    if (drive.IsReady)
+    {
+        // drives.AddRow(NAME, TYPE, FORMAT, SIZE(BYTES), FREE SPACE);
+        drives.AddRow(drive.Name, drive.DriveType.ToString(), drive.DriveFormat, drive.TotalSize.ToString("N0"), drive.AvailableFreeSpace.ToString("N0"));
+    }
+    else
+    {
+        drives.AddRow(drive.Name, drive.DriveType.ToString(), string.Empty, string.Empty, string.Empty);
+    }
+}
+
+AnsiConsole.Write(drives);
+
+#endregion
+
+
+#region Managing directories
+
+SectionTitle("Managing directories");
+
+string newFolder = Combine(
+  GetFolderPath(SpecialFolder.Personal), "NewFolder");
+
+WriteLine($"Working with: {newFolder}");
+
+// We must explicitly say which Exists method to use 
+// because we statically imported both Path and Directory.
+WriteLine($"Does it exist? {Path.Exists(newFolder)}");
+
+WriteLine("Creating it...");
+CreateDirectory(newFolder);
+
+// Let's use the Directory.Exists method this time.
+WriteLine($"Does it exist? {Directory.Exists(newFolder)}");
+Write("Confirm the directory exists, and then press any key.");
+ReadKey(intercept: true);
+WriteLine();
+
+WriteLine("Deleting it...");
+Delete(newFolder, recursive: true);
+WriteLine($"Does it exist? {Path.Exists(newFolder)}");
+
+#endregion
+
+
+#region Managing files
+
+SectionTitle("Managing files");
+
+// Define a directory path to output files starting
+// in the user's folder.
+string dir = Combine(
+  GetFolderPath(SpecialFolder.Personal), "OutputFiles");
+
+CreateDirectory(dir);
+
+// Define file paths.
+string textFile = Combine(dir, "Dummy.txt");
+string backupFile = Combine(dir, "Dummy.bak");
+WriteLine($"Working with: {textFile}");
+
+WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+// Create a new text file and write a line to it.
+StreamWriter textWriter = File.CreateText(textFile);
+textWriter.WriteLine("Hello, C#!");
+textWriter.Write("Write only writes a char? No!");
+textWriter.Close(); // Close file and release resources.
+WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+// Copy the file, and overwrite if it already exists.
+File.Copy(sourceFileName: textFile, destFileName: backupFile, overwrite: true);
+
+WriteLine($"Does {backupFile} exist? {File.Exists(backupFile)}");
+
+Write("Confirm the files exist, and then press any key.");
+ReadKey(intercept: true);
+WriteLine();
+
+// Delete the file.
+File.Delete(textFile);
+WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+// Read from the text file backup.
+WriteLine($"Reading contents of {backupFile}:");
+StreamReader textReader = File.OpenText(backupFile);
+WriteLine(textReader.ReadToEnd());
+textReader.Close();
+
+#endregion
+
+
+#region Managing Paths
+SectionTitle("Managing Paths");
+
+WriteLine($"Folder Name: {GetDirectoryName(textFile)}");
+WriteLine($"File Name: {GetFileName(textFile)}");
+WriteLine("File Name without Extension: {0}", GetFileNameWithoutExtension(textFile));
+WriteLine($"File Extension: {GetExtension(textFile)}");
+WriteLine($"Random File Name: {GetRandomFileName()}");
+WriteLine($"Temporary File Name: {GetTempFileName()}");
+
+#endregion
+
+
+#region Getting File Information
+SectionTitle("Getting File Information");
+
+FileInfo info = new FileInfo(backupFile); // new(backupFile); also works as FileInfo is explicitly defined.
+WriteLine($"{backupFile}:");
+WriteLine($"  Contains {info.Length} bytes.");
+WriteLine($"  Last accessed on {info.LastAccessTime}.");
+WriteLine($"  Attributes: {info.Attributes}.");
+WriteLine($"  Has Read-Only set to: {info.IsReadOnly}.");
+
+#endregion
