ec6834ac2ff299f6211176f149b60c48b6430a3a
AlexLenitschek <alexander.lenitschek@gmx.de>
2025-02-23 21:56:23 +0100

    Adding Serialization with Xml and Json

---
+++ WorkingWithSerialization/Program.Helpers.cs
@@ -0,0 +1,24 @@
+﻿#region Serializing as XML
+partial class Program
+{
+    private static void SectionTitle(string title)
+    {
+        ConsoleColor previouscolor = ForegroundColor;
+        ForegroundColor = ConsoleColor.Yellow;
+        WriteLine($"*** {title} ***");
+        ForegroundColor = previouscolor;
+    }
+
+    private static void OutputFileInfo(string path)
+    {
+        WriteLine("**** File Info ****");
+        WriteLine($"File: {GetFileName(path)}");
+        WriteLine($"Path: {GetDirectoryName(path)}");
+        WriteLine($"Size: {new FileInfo(path).Length:N0} bytes.");
+        WriteLine("/------------------");
+        WriteLine(File.ReadAllText(path));
+        WriteLine("------------------/");
+    }
+}
+
+#endregion
